edd500d305c5f8a221fc84511b21e2d90368f9a0
Thuong Hoai <ht2522001@gmail.com>
2024-12-09 20:55:15 +0700

    config tailwind

--- examples/example-phase1/example-phase1/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <html lang="en">
-    <body>{children}</body>
-  </html>
-);
-
-export default RootLayout;
