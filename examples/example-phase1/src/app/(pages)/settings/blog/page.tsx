01c7eefe6de6890362f16ec1827acf124d485301
Thuong Hoai <ht2522001@gmail.com>
2024-12-11 21:04:07 +0700

    example with slug

--- examples/example-phase1/src/app/(pages)/settings/blog/page.tsx
+++ examples/example-phase1/src/app/(pages)/settings/blog/page.tsx
@@ -1,6 +1,13 @@
 
-const Blog = () => {
-    return <div>Blog page</div>;
+const Blog = ({ params }: any) => {
+    const { slug } = params;
+
+    return (
+        <>
+            <h1>Blog page</h1>
+            <div>Blog: {slug}</div>
+        </>
+    );
 };
 
 export default Blog;
