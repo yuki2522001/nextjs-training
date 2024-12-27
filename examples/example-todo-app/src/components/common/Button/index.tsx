16a8d7d61885635386345291509bf1f3274d9367
yuki2522001 <tam.truong@dfinsolutions.com>
2024-12-27 17:27:29 +0700

    implementing component button

---
+++ examples/example-todo-app/src/components/common/Button/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+
+const ButtonComponent = () => {
+    return (
+        <Button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-danger">Delete</Button>
+    )
+}
+
+export default ButtonComponent;
