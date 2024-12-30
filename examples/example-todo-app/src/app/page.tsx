646f3a8eb5e510b6f244968ea3df5ac28f6fef57
yuki2522001 <tam.truong@dfinsolutions.com>
2024-12-30 15:22:37 +0700

    refactor custom button

--- examples/example-todo-app/src/app/page.tsx
+++ examples/example-todo-app/src/app/page.tsx
@@ -1,23 +1,15 @@
+'use client';
+
 import { Button } from "@/components";
 
-const Home = () => {
-  //TODO: Just show UI to reivew
-  return (
-    <div className="p-10 w-96 flex justify-between">
-      <Button
-        variant="info"
-        children='SAVE' loading={false} />
-      <Button
-        variant="success"
-        children='FINISHED' loading={false} />
-      <Button
-        variant="warning"
-        children='GET TASKS' loading={false} />
-      <Button
-        variant="danger"
-        children='DELETE' loading={false} />
-    </div>
-  );
-}
+const Home = () => (
+  // TODO: Just show UI to review
+  <div className="flex justify-between w-96 p-10">
+    <Button color="info">Info</Button>
+    <Button color="success">Success</Button>
+    <Button color="warning">Warning</Button>
+    <Button color="danger">Danger</Button>
+  </div>
+);
 
 export default Home;
--- examples/example-todo-app/src/components/common/Button/index.tsx
+++ examples/example-todo-app/src/components/common/Button/index.tsx
@@ -1,58 +1,27 @@
-'use client'
+'use client';
 
-import { Spinner } from '@nextui-org/react';
-// Libs
-import { ReactNode } from 'react';
+import { extendVariants, Button as ButtonNextUI } from '@nextui-org/react';
 
-type ButtonProps = {
-  children: ReactNode;
-  type?: 'button' | 'submit';
-  variant?:
-    | 'info'
-    | 'success'
-    | 'danger'
-    | 'warning';
-  customClass?: string;
-  name?: string;
-  value?: string;
-  ariaLabel?: string;
-  disabled?: boolean;
-  loading: boolean;
-  onClick?: () => void;
-}
+export const Button = extendVariants(ButtonNextUI, {
+  variants: {
+    color: {
+      danger: 'bg-danger text-white',
+      warning: 'bg-warning text-white',
+      success: 'bg-success text-red',
+      info: 'bg-info text-dark',
+    },
+    radius: {
+      xs: 'rounded',
+    },
+    size: {
+      default: 'text-xxs text-accent-secondary px-2.5 py-2',
+    },
+  },
+  defaultVariants: {
+    color: 'info',
+    radius: 'xs',
+    size: 'default',
+  },  
+});
 
-const ButtonComponent = ({
-  children,
-  type = 'button',
-  variant = 'info',
-  customClass = '',
-  name,
-  value,
-  ariaLabel = 'button',
-  disabled = false,
-  loading,
-  onClick
-}: ButtonProps) => {
-  const variantClasses: Record<string, string> = {
-    info: 'bg-info',
-    success: 'bg-success',
-    danger: 'bg-danger',
-    warning: 'bg-warning'
-  };
-
-    return (
-      <button
-        name={name}
-        type={type}
-        value={value}
-        aria-label={ariaLabel}
-        disabled={disabled}
-        className={`${variantClasses[variant]} ${customClass} text-xxs rounded-default text-accent-secondary px-2.5 py-2`}
-        onClick={onClick}
-      >
-        {loading ? <Spinner /> : children}
-      </button>
-    )
-}
-
-export default ButtonComponent;
+export default Button;
