7ed37133b20e57f4f6820742ce4edfab25488001
yuki2522001 <tam.truong@dfinsolutions.com>
2024-12-30 15:23:45 +0700

    update value of button

--- examples/example-todo-app/src/app/page.tsx
+++ examples/example-todo-app/src/app/page.tsx
@@ -5,10 +5,10 @@ import { Button } from "@/components";
 const Home = () => (
   // TODO: Just show UI to review
   <div className="flex justify-between w-96 p-10">
-    <Button color="info">Info</Button>
-    <Button color="success">Success</Button>
-    <Button color="warning">Warning</Button>
-    <Button color="danger">Danger</Button>
+    <Button color="info">SAVE</Button>
+    <Button color="success">FINISHED</Button>
+    <Button color="warning">GET TASKS</Button>
+    <Button color="danger">DELETE</Button>
   </div>
 );
 
