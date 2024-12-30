9a3d8e48e35c97fb4ca2f95fa7586b1bcf078f97
yuki2522001 <tam.truong@dfinsolutions.com>
2024-12-30 10:18:45 +0700

    implementing button common component

--- examples/example-todo-app/src/app/page.tsx
+++ examples/example-todo-app/src/app/page.tsx
@@ -1,8 +1,22 @@
 import { Button } from "@/components";
 
 const Home = () => {
+  //TODO: Just show UI to reivew
   return (
-    <Button />
+    <div className="p-10 w-96 flex justify-between">
+      <Button
+        variant="info"
+        children='SAVE' loading={false} />
+      <Button
+        variant="success"
+        children='FINISHED' loading={false} />
+      <Button
+        variant="warning"
+        children='GET TASKS' loading={false} />
+      <Button
+        variant="danger"
+        children='DELETE' loading={false} />
+    </div>
   );
 }
 
--- examples/example-todo-app/src/components/common/Button/index.tsx
+++ examples/example-todo-app/src/components/common/Button/index.tsx
@@ -1,10 +1,57 @@
 'use client'
 
-import { Button } from '@nextui-org/react'
+import { Spinner } from '@nextui-org/react';
+// Libs
+import { ReactNode } from 'react';
 
-const ButtonComponent = () => {
+type ButtonProps = {
+  children: ReactNode;
+  type?: 'button' | 'submit';
+  variant?:
+    | 'info'
+    | 'success'
+    | 'danger'
+    | 'warning';
+  customClass?: string;
+  name?: string;
+  value?: string;
+  ariaLabel?: string;
+  disabled?: boolean;
+  loading: boolean;
+  onClick?: () => void;
+}
+
+const ButtonComponent = ({
+  children,
+  type = 'button',
+  variant = 'info',
+  customClass = '',
+  name,
+  value,
+  ariaLabel = 'button',
+  disabled = false,
+  loading,
+  onClick
+}: ButtonProps) => {
+  const variantClasses: Record<string, string> = {
+    info: 'bg-info',
+    success: 'bg-success',
+    danger: 'bg-danger',
+    warning: 'bg-warning'
+  };
+
     return (
-      <Button type="submit" className="btn bg-danger text-xxs rounded-default text-accent-secondary px-2.5 py-2">DELETE</Button>
+      <button
+        name={name}
+        type={type}
+        value={value}
+        aria-label={ariaLabel}
+        disabled={disabled}
+        className={`${variantClasses[variant]} ${customClass} text-xxs rounded-default text-accent-secondary px-2.5 py-2`}
+        onClick={onClick}
+      >
+        {loading ? <Spinner /> : children}
+      </button>
     )
 }
 
