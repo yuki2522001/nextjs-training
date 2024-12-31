f37a959a44bc4d413fad3ce6d793c010aaf26f4e
yuki2522001 <tam.truong@dfinsolutions.com>
2024-12-31 17:02:04 +0700

    create a common table component

---
+++ examples/example-todo-app/src/constants/table.ts
@@ -0,0 +1,27 @@
+export const columns = [
+    { name: ".No", uid: "no" },
+    { name: "Todo Item", uid: "todoItem" },
+    { name: "Status", uid: "status" },
+    { name: "Actions", uid: "actions" },
+];
+
+export const rows = [
+    {
+        id: 1,
+        no: 1,
+        task: "Implementing a common button component",
+        status: "In progress",
+    },
+    {
+        id: 2,
+        no: 2,
+        task: "Implementing a common input component",
+        status: "In progress",
+    },
+    {
+        id: 3,
+        no: 3,
+        task: "Implementing a common table component",
+        status: "In progress",
+    }
+]
